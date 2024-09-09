979de819bcf885930636ee6a8d654651da0c7ed5
AnaKaroline-23 <anakarolinesoraes.23@gmail.com>
2024-09-09 08:40:36 -0300

    atividade04

---
+++ atividade04.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+nota01 = float(input(" nota01 "))
+nota02 = float(input(" nota02 "))
+nota03 = float(input(" nota03 "))
+soma = (nota01+nota02+nota03)/3
+print (soma)
+
